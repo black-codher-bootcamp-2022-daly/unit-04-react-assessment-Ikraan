9dc104de8e26edf9b7c53abf6ec8523d879c2463
Ikraan <ikram2009@hotmail.com>
2023-01-16 02:12:31 +0000

    changes to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ function Header (props) {
     return (
         <div id="header"><h1>Media Store 🎼</h1>
         <div>
-        <link to="/" className="Link" id="homelink">Home</link> |
-        <link to="/about" className="Link" id="aboutlink">About</link> |
-        <link to="/basket" className="Link" id="basketlink">Basket {props.basketCount} item {props.basketCount===1?"":"s"}</link>
+        <link to="/" className="Link" id="homelink"><h1>Home</h1></link> 
+        <link to="/about" className="Link" id="aboutlink"><h1>About</h1></link> |
+        <link to="/basket" className="Link" id="basketlink"><h1>Basket: {props.basketCount} </h1></link>
         </div></div>
     )
 }
