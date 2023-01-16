9aa2893555575b3542ed7fdf125a9761e748e23d
Ikraan <ikram2009@hotmail.com>
2023-01-16 00:17:39 +0000

    fixing bugs

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -9,14 +9,28 @@ const Basket = ({ basket, basketTotal,...props }) => {
   // console.log(basket);
 
   return (
-    <div className="basket"> <h1>Basket: </h1>
-    <BasketCount basketCount={count} />
-    { (count > 0 ? 
-    basket.map(item => <Product key={item.trackId} item={item} {...props} /> ):
-           <div className="empty">Sorry, no items in basket...</div>)}
-           <BasketTotal basketTotal={basketTotal} />
-    </div>
-  );}
+    <>
+      {!basket || basket.length === 0 ? (
+        <div className="empty">Sorry, no items in basket...</div>
+      ) : (
+        basket.map((item) => (
+          <div className="product" key={item.trackId}>
+            <Product
+              item={item}
+              kind={item.kind}
+              name={item.trackName}
+              thumbnail={item.artworkUrl100}
+              currency={item.currency}
+              price={item.trackPrice}
+              inBasket={item.inBasket}
+              removeFromBasket={removeFromBasket}
+            />
+          </div>
+        ))
+      )}
+    </>
+  );
+};
 
 Basket.propTypes = {
   basket: PropTypes.array.isRequired
