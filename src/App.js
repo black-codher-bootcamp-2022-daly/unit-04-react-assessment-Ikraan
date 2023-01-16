10a3f47e56402dbd34e0c7a316a5f714b4658839
Ikraan <ikram2009@hotmail.com>
2023-01-16 01:34:14 +0000

    debugging App.js

--- src/App.js
+++ src/App.js
@@ -15,53 +15,85 @@ import Pagination from './components/Pagination';
 function App() {
 
   const itemsPerPage = 4;
-  const [items, setItems] = useState(data);
+  const [items, setItems] = useState(data.slice(0,10));
   const [basket, setBasket] = useState([]);
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState(' ');
   const [total, setTotal] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const addToBasket = (id) => {
-    setBasket(basket.concat(items.filter(item => item.trackId === id)));
-    setItems([...items.map(item => {
-      if (item.trackId === id) {
-        item.inBasket = true;
-        setTotal(total + item.trackPrice);
+  function addToBasket(trackId) {
+    products.forEach((product) => {
+      if (product.trackId === trackId) {
+        product.inBasket = true;
+        setBasket((prev) => [...prev, product]);
+
+        if (product.trackPrice) {
+          setTotal(parseFloat(total + product.trackPrice));
+        } else {
+          setTotal(total + 0);
+        }
       }
-      return item;
-    }
-    )]);
+      setCount(count + 1);
+    });
   }
-
-  const removeFromBasket = (id) => {
-    setBasket(basket.filter(item => item.trackId !== id));
-    setItems([...items.map(item => {
-      if (item.trackId === id) {
-        item.inBasket = false;
-        setTotal(total - item.trackPrice);
+  
+  function removeFromBasket(trackId) {
+    const removeFromCart = [];
+    basket.forEach((products) => {
+      if (products.trackId !== trackId) {
+        removeFromCart.push(products);
+      } else {
+        products.inBasket = !products.inBasket;
+        if (products.trackPrice) {
+          setTotal(parseFloat(total - products.trackPrice));
+        }
+        return products;
       }
-      return item;
-    }
-    )]);
+    });
+
+    setBasket(removeFromCart);
+    setCount(count - 1);
   }
 
   async function search(value) {
+    console.log ("find items that's been clicked", value);
+
     const results = await fetch(`https://itunes.apple.com/search?term=${value}&limit=50&explicit=no`).then(res => res.json());
     if (!results.error) {
       setItems(results.results.filter(result => result.trackName && basket.findIndex(item => result.id === item.trackId) === -1));
     }
   }
 
-  useEffect(() => {
-    let basketCountLabel = `Basket: ${basket.length} item` + (basket.length === 1 ? "" : "s");
-    if (document) {
-      document.title = basketCountLabel;
-      document.getElementById("basketlink").innerText = basketCountLabel;
-    }
-  });
+  function BasketList() {
+    return(
+      <>
+      <BasketCount />
+      <Basket 
+      basket={basket}
+      addToBasket={addToBasket}
+      removeFromBasket={removeFromBasket}
+      basketTotal={total}
+      BasketCount={count} />
+
+      <div className='Price-total'>
+        Total Price <BasketTotal basketTotal={total} />
+      </div>
+      </>
+    );
+  }
+
+  // useEffect(() => {
+  //   let basketCountLabel = `Basket: ${basket.length} item` + (basket.length === 1 ? "" : "s");
+  //   if (document) {
+  //     document.title = basketCountLabel;
+  //     document.getElementById("basketlink").innerText = basketCountLabel;
+  //   }
+  // });
 
   return (
     <Router>
       <div className="container">
+        <h1> Media Store</h1>
         <Routes>
           <Route exact path="/" element={
             <Fragment>
--- src/components/Basket.js
+++ src/components/Basket.js
@@ -3,33 +3,37 @@ import Product from "./Product";
 import BasketCount from "./BasketCount";
 import BasketTotal from "./BasketTotal";
 
-const Basket = ({ basket, basketTotal,...props }) => {
+function Basket ({ basket, basketCount,basketTotal,removeFromBasket,...props }) {
   // const { removeFromBasket } = props;
   const count = basket = basket.length;
   // console.log(basket);
 
   return (
-    <>
+    <div>
+      <h1>Basket</h1>
+
+      <BasketCount basketCount={basketCount} />
       {!basket || basket.length === 0 ? (
         <div className="empty">Sorry, no items in basket...</div>
       ) : (
-        basket.map((item) => (
-          <div className="product" key={item.trackId}>
+        basket.map((product) => (
+          <div className="basket" key={product.trackId}>
             <Product
-              item={item}
-              kind={item.kind}
-              name={item.trackName}
-              thumbnail={item.artworkUrl100}
-              currency={item.currency}
-              price={item.trackPrice}
-              inBasket={item.inBasket}
+              product={product}
+              key={product.trackId}
+              inBasket={product.inBasket}
               removeFromBasket={removeFromBasket}
             />
           </div>
         ))
       )}
-    </>
+
+      <h2 className="basket-total">
+        Total: £
+        {basketTotal === 0 ? `0:00` : <BasketTotal basketTotal={basketTotal} />}
+      </h2>
+    </div>
   );
-};
+}
 
 export default Basket;
