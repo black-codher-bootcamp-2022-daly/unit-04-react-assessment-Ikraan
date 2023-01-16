969b19c68f7cad45959aac35e3d0ce0f206cbd5b
Ikraan <ikram2009@hotmail.com>
2023-01-16 01:57:56 +0000

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -15,74 +15,109 @@ import Pagination from './components/Pagination';
 function App() {
 
   const itemsPerPage = 4;
-  const [item, setItem] = useState(data.slice(0,10));
+  const [product, setProducts] = useState(data.slice(0,10));
   const [basket, setBasket] = useState([]);
   const [term, setTerm] = useState(' ');
   const [total, setTotal] = useState(0);
   const [count, setCount] = useState(0);
 
-  const addToBasket = (id) => {
-    setBasket(basket.concat(items.filter(item => item.trackId === id)));
-    setItems([...items.map(item => {
-      if (item.trackId === id) {
-        item.inBasket = true;
-        setTotal(total + item.trackPrice);
-      }
-      return item;
-    }
-    )]);
-  }
+  function addToBasket(trackId) {
+   
+    product.forEach((items) => {
+      if (items.trackId === trackId) {
+        product.inBasket = true;
+        setBasket((prev) => [...prev, product]);
 
-  const removeFromBasket = (id) => {
-    setBasket(basket.filter(item => item.trackId !== id));
-    setItems([...items.map(item => {
-      if (item.trackId === id) {
-        item.inBasket = false;
-        setTotal(total - item.trackPrice);
+        if (product.trackPrice) {
+          setTotal(parseFloat(total + product.trackPrice));
+        } else {
+          setTotal(total + 0);
+        }
       }
-      return item;
-    }
-    )]);
+      setCount(count + 1);
+    });
+  }
+  
+  function removeFromBasket(trackId) {
+    const removeFromCart = [];
+    basket.forEach((products) => {
+      if (products.trackId !== trackId) {
+        removeFromCart.push(products);
+      } else {
+        products.inBasket = !products.inBasket;
+        if (products.trackPrice) {
+          setTotal(parseFloat(total - products.trackPrice));
+        }
+        return products;
+      }
+    });
+
+    setBasket(removeFromCart);
+    setCount(count - 1);
   }
 
   async function search(value) {
-    const results = await fetch(`https://itunes.apple.com/search?term=${value}&limit=50&explicit=no`).then(res => res.json());
+    console.log("find books that got clicked", value);
+    const url = `https://itunes.apple.com/search?term=${value}&limit=30&explicit=no`;
+
+    const results = await fetch(url).then((res) => res.json());
     if (!results.error) {
-      setItems(results.results.filter(result => result.trackName && basket.findIndex(item => result.id === item.trackId) === -1));
+      setProducts(results.results.filter((result) => result.trackName));
     }
   }
-  useEffect(() => {
-    let basketCountLabel = `Basket: ${basket.length} item` + (basket.length === 1 ? "" : "s");
-    if (document) {
-      document.title = basketCountLabel;
-      document.getElementById("basketlink").innerText = basketCountLabel;
-    }
-  });
+
+  function BasketList() {
+    return(
+      <>
+      <BasketCount />
+      <Basket 
+      basket={basket}
+      addToBasket={addToBasket}
+      removeFromBasket={removeFromBasket}
+      basketTotal={total}
+      BasketCount={count} />
+
+      <div className='Price-total'>
+        Total Price <BasketTotal basketTotal={total} />
+      </div>
+      </>
+    );
+  }
+
+  // useEffect(() => {
+  //   let basketCountLabel = `Basket: ${basket.length} item` + (basket.length === 1 ? "" : "s");
+  //   if (document) {
+  //     document.title = basketCountLabel;
+  //     document.getElementById("basketlink").innerText = basketCountLabel;
+  //   }
+  // });
+  function Home() {
+    return (
+
+      <>
+        <Search term={term} setTerm={setTerm} handleSubmit={search} />
+        <ProductList
+          product={product}
+          addToBasket={addToBasket}
+          removeFromBasket={removeFromBasket}
+          basketCount={data.length}
+        />
+      </>
+    );
+  }
 
   return (
-    <Router>
-      <div className="container">
-        <h1> Media Store</h1>
+    <div className="container">
+      <h1>Media Store App</h1>
+      <BrowserRouter>
+        <Header itemCount={count} />
         <Routes>
-          <Route exact path="/" element={
-            <Fragment>
-              <Header />
-              <Search term={term} search={search} setTerm={setTerm} />
-              <ProductList items={items} addToBasket={addToBasket} removeFromBasket={removeFromBasket} itemCount={items.length} />
-              <Pagination itemsPerPage={itemsPerPage} item={items.length} />
-            </Fragment>
-          } />
-          <Route path="/basket" element={
-            <Fragment>
-              <Header />
-              <Basket basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} basketTotal={total} />
-            </Fragment>
-          } />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="basket" element={<Basketpage />} />
         </Routes>
-      </div>
-    </Router>
+      </BrowserRouter>
+    </div>
   );
 }
-
 export default App;
