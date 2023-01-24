5094845ce5c2f3ea07a7dcd0206f047d53b7a721
Ikraan Ahmed <109916253+Ikraan@users.noreply.github.com>
2023-01-24 09:09:33 +0000

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Header(props) {
-    return (
-      <div id="navigation">
-        <h1 className="title"> Media Store 🎼</h1>
-        <div>
-          <Link className="link" to="/" id="homelink">
-            <h4>Home</h4>
-          </Link>
+const Header = (props) => {
+  console.log(props.basketCount);
+  return (
+    <div id="header">
+        <h1>Media Store <span id="icon">🎼</span> <br></br></h1>
+        <div id="nav">
+          <Link to="/"> Home </Link> |
+          <Link to="/about"> About </Link> |
+          <Link to="/basket" id="basketlink"> Basket: {props.basketCount} item{props.basketCount===1?"":"s"}</Link>
         </div>
-        <div>
-          <Link className="link" to="/about" id="aboutlink">
-            <h4>About </h4></Link>
-        </div>
-        <div>
-          <Link className="link" to="/basket" id="basketlink">
-            <h4>Basket:{props.itemCount}</h4>
-          </Link>
-        </div></div> )}
+    </div>
+  );
+}
 
 export default Header;
