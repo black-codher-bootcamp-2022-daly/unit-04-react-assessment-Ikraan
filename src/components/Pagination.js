186e8d922a654a42f4c047d209799bb11c25ff4e
Ikraan Ahmed <109916253+Ikraan@users.noreply.github.com>
2023-01-24 09:13:17 +0000

    Updated pagination.js but commented due to unknown issues

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import Product from './Product';
+// import React from "react";
+// import Product from "./Product";
 
-const Pagination = ({ postsPerPage, totalPosts, nextPage, previousPage }) => {
-    const pageNumbers = [];
-  
-    for (let index = 1; index <= Math.ceil(totalPosts / postsPerPage); index++) {
-      pageNumbers.push(index);
-    }
-  
-    return (
-      <div className="pag-container">
-        <ul className="paginate">
-          <li onClick={previousPage} className="page-number">
-            ...Previous page
-          </li>
-          {pageNumbers.map((number) => (
-            <li
-              key={number}
-              onClick={() => Pagination(number)}
-              className="page-number"
-            >
-              {number}
-            </li>
-          ))}
-          <li onClick={nextPage} className="page-number">
-            Next page...
-          </li>
-        </ul>
-      </div>
-    );
-  };
+// export const Posts = ({ posts, loading }) => {
+//   if (loading) {
+//     return <h2>Loading...</h2>;
+//   }
+//   return (
+//     <ul className="lists mb-4">
+//       {posts.map((post) => (
+//         <li key={trackId} className="lists-item">
+//           {trackName}
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
 
-  export default Pagination;
+// const Pagination = ({ postsPerPage, totalPosts, nextPage, previousPage }) => {
+//   const pageNumbers = [];
+
+//   for (let index = 1; index <= Math.ceil(totalPosts / postsPerPage); index++) {
+//     pageNumbers.push(index);
+//   }
+
+//   return (
+//     <ul className="paginate">
+//       {pageNumbers.map((number) => (
+//         <li key={number} className="page-item">
+//           <a
+//             onClick={() => Pagination(number)}
+//             href="!#"
+//             className="page-link"
+//           ></a>
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default Pagination;
