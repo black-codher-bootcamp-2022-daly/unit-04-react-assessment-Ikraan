8a42e80268ebe2cbc9b0439422addfb0720707b3
Ikraan Ahmed <109916253+Ikraan@users.noreply.github.com>
2023-01-24 09:20:24 +0000

    Updated About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { link } from 'react-router-dom';
-import Header from '../components/Header';
+import { Link } from 'react-router-dom';
 
-function About(){
+
+
+
+const About= (props) => {
     return(
         <>
-            <h1 className='title'>About</h1>
+       <div id="nav">
+          <Link to="/"> Home </Link> |
+          <Link to="/about"> About </Link> |
+          <Link to="/basket" id="basketlink"> Basket: {props.basketCount} item{props.basketCount===1?"":"s"}</Link>
+        </div>
+        <div> <h1 className='title'>About</h1> </div>
+        <hr></hr>
         <div className='aboutBody'>
-                <h2 className='aboutTitle'>Welcome to The Music Store 🎼</h2>
+                <h2 className='aboutTitle'>Welcome to The Music Store <span id='icon'>🎼</span></h2>
                 <p> This Application was created by Ikraan Ahmed, student at Blackcodher Full stack Development Bootcamp.<br /> 
                 This is a Mediastore App designed to display a list of media that enables users to add to their baskets. <br /> 
-                Users can browse the itunes store for media that users are interested in. <br /> 
-                Users can use the search bar to find the latest media content by Artist name or track, add to basket and also keep track of costs. <br /> </p>
+                Users can browse the media store for media that users are interested in. <br /> 
+                Users can use the search bar to find the latest media content by Artist name or track, add to their basket and also keep track of costs. <br /> </p>
             </div>
-            </>
+            
 
-        
+        </>
     )
 } 
 
