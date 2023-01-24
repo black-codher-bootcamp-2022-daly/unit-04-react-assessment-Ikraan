49fdb2a0f1f76b1dd0f21fb77a0d10ffae79af44
Ikraan Ahmed <109916253+Ikraan@users.noreply.github.com>
2023-01-24 09:23:14 +0000

    Updated App.js commented out pagination

--- src/App.js
+++ src/App.js
@@ -1,123 +1,150 @@
-import './styles/App.css';
-import React, {Fragment, useEffect, useState} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './styles/App.css';
-import data from './models/data.json';
-import ProductList from './components/ProductList';
-import Search from './components/Search';
-import Basket from './components/Basket';
-import BasketCount from './components/BasketCount';
-import Header from './components/Header';
-import About from './pages/About';
-import BasketTotal from './components/BasketTotal';
-import Pagination from './components/Pagination';
+import "./styles/App.css";
+import React, { Fragment, useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import data from "./models/data.json";
+import ProductList from "./components/ProductList";
+import Search from "./components/Search";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import About from "./pages/About";
+// import Pagination from "./components/Pagination";
+// import axios from "axios";
+// import { Posts } from "./components/Pagination";
+
+
+
+
 
 function App() {
-  const [product, setProducts] = useState(data.slice(0,10));
+  const [items, setItems] = useState(data);
   const [basket, setBasket] = useState([]);
-  const [term, setTerm] = useState(' ');
+  const [term, setTerm] = useState("");
   const [total, setTotal] = useState(0);
-  const [count, setCount] = useState(0);
-  const [loadMore, setLoadMore] = useState(true);
-  function moreData(currentCount) {
-    if (currentCount ===data.length - 10) setLoadMore(false);
-    return setProducts((currentItems) => [
-      ...currentItems,
-      ...data.slice(currentCount, currentCount + 10),
-    ])
-  }
-  useEffect(()=> {
-  }, [term]);
-  function addToBasket(trackId) {
-   
-    product.forEach((items) => {
-      if (items.trackId === trackId) {
-        product.inBasket = true;
-        setBasket((prev) => [...prev, product]);
-        if (product.trackPrice) {
-          setTotal(parseFloat(total + product.trackPrice));
-        } else {
-          setTotal(total + 0);
+  // const [loading, setLoading] = useState(false);
+  // const [currentPage, setCurrentPage] = useState(1);
+  // const [postsPerPage] = useState(20);
+
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     setLoading(true);
+  //     const res = await axios.get("./models/data.json");
+  //     setPosts(res.data);
+  //     setLoading(false);
+  //   };
+  //   fetchPosts();
+  // }, []);
+
+  // const indexOfLastPage = currentPage * postsPerPage;
+  // const indexOfFirstPage = indexOfLastPage - postsPerPage;
+  // const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPage);
+
+  // const Pagination = () => setCurrentPage(pageNumber);
+
+  const addToBasket = (id) => {
+    setBasket(basket.concat(items.filter((item) => item.trackId === id)));
+    setItems([
+      ...items.map((item) => {
+        if (item.trackId === id) {
+          item.inBasket = true;
+          setTotal(total + item.trackPrice);
         }
-      }
-      setCount(count + 1);
-    });
-  }
-  
-  function removeFromBasket(trackId) {
-    const removeFromCart = [];
-    basket.forEach((product) => {
-      if (product.trackId !== trackId) {
-        removeFromCart.push(product);
-      } else {
-        product.inBasket = !product.inBasket;
-        if (product.trackPrice) {
-          setTotal(parseFloat(total - product.trackPrice));
+        return item;
+      }),
+    ]);
+  };
+
+  const removeFromBasket = (id) => {
+    setBasket(basket.filter((item) => item.trackId !== id));
+    setItems([
+      ...items.map((item) => {
+        if (item.trackId === id) {
+          item.inBasket = false;
+          setTotal(total - item.trackPrice);
         }
-        return product;
-      }
-    });
-    setBasket(removeFromCart);
-    setCount(count - 1);
-  }
+        return item;
+      }),
+    ]);
+  };
+
   async function search(value) {
-    console.log("find books that got clicked", value);
-    const results = await fetch(`https://itunes.apple.com/search?term=${value}&limit=30&explicit=no`).then((res) => res.json());
+    const results = await fetch(
+      `https://itunes.apple.com/search?term=${value}&limit=50&explicit=no`
+    ).then((res) => res.json());
     if (!results.error) {
-      setProducts(
+      setItems(
         results.results.filter(
           (result) =>
             result.trackName &&
-            basket.findIndex((product) => result.id === product.trackId) === -1
+            basket.findIndex((item) => result.id === item.trackId) === -1
         )
       );
     }
   }
-  function BasketList() {
-    return(
-      <>
-      <BasketCount />
-      <Basket 
-      basket={basket}
-      addToBasket={addToBasket}
-      removeFromBasket={removeFromBasket}
-      basketTotal={total}
-      basketCount={count} />
-      </>
-    );
-  }
-  function Home() {
-    return (
-      <Prop>
-        <Search term={term} setTerm={setTerm} search={search} />
-        <ProductList
-          items={product}
-          addToBasket={addToBasket}
-          removeFromBasket={removeFromBasket}
-          basketCount={data.length}
-        />
-        {loadMore && (
-          <button
-            className="load-more-button"
-            onClick={() => moreData(product.length)}
-          >
-            Load More Products
-          </button>
-        )}
-      </Prop>
-    );
-  }
+
+  useEffect(() => {
+    let basketCount =
+      `Basket: ${basket.length} item` + (basket.length === 1 ? "" : "s");
+    document.title = basketCount;
+    document.getElementById("basketlink").innerText = basketCount;
+  });
+
+  // function addpage() {
+  //   setPostsPerPage(nextPage + 1);
+  // }
+
+  // function minusPage() {
+  //   setPostsPerPage(previousPage - 1);
+  // }
+
   return (
-    <Router>
-      <div className="App">
-        <Header itemCount={count} />
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/basket" element={<BasketList />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Fragment>
+                  <img src="headerimg.jpg" alt="img" className="banner" />
+                  <Header />
+                  <Search term={term} search={search} setTerm={setTerm} />
+                  <ProductList
+                    items={items}
+                    addToBasket={addToBasket}
+                    removeFromBasket={removeFromBasket}
+                    itemCount={items.length}
+                  />
+                </Fragment>
+              }
+            />
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/basket"
+              element={
+                <Fragment>
+                  <Header />
+                  <Basket
+                    basket={basket}
+                    addToBasket={addToBasket}
+                    removeFromBasket={removeFromBasket}
+                    basketTotal={total}
+                  />
+                  {/* <Posts posts={currentPosts} loading={loading} />
+                  <Pagination
+                    postsPerPage={postsPerPage}
+                    totalPosts={posts.length}
+                    Pagination={Pagination}
+                  /> */}
+                </Fragment>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
+
 export default App;
